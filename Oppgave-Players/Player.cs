12a90ad37668d541e8199a9024fa59dba7ef4504
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-13 13:00:34 +0100

    Add project files.

---
+++ Oppgave-Players/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Oppgave_Players
+{
+    public class Player
+    {
+        private string _name;
+        private int _points; 
+        private Random _random;
+
+        public Player(string name, int points, Random random)
+        {
+            _name = name;
+            _points = points;
+            _random = random;
+        }
+
+
+
+        public void Play(Player player2)
+        {
+            var winner = _random.Next(2) == 0 ? this : player2;
+            var loser = winner == this ? player2 : this;
+            winner._points += 1;
+            loser._points -= 1;
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine(_name.PadRight(12) + _points.ToString().PadLeft(3));
+        }
+    }
+}
---
+++ Oppgave-Players/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Oppgave_Players
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var random = new Random();
+
+            var players = new[] 
+                {   new Player("Per", 10, random), 
+                    new Player("Pål", 10, random), 
+                    new Player("Espen", 10, random)
+                };
+
+            for (var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = random.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+                player1.Play(player2);
+                //await Task.Delay(10);
+
+            }
+
+            foreach (var player in players)
+            {
+                player.ShowNameAndPoints();
+            }
+          
+
+
+
+
+        }
+    }
+}
